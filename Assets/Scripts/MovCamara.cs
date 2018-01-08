0b87baf6d81c3a217bca2ad3c550dfc3cfd685d4
Sergi Cugat <sergi.cugat@estudiants.urv.cat>
2018-01-08 22:47:33 +0100

    Camara ortografica i llums posades

--- Assets/Scripts/MovCamara.cs
+++ Assets/Scripts/MovCamara.cs
@@ -15,7 +15,7 @@ public class MovCamara : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) && transform.position.y < limitDalt) transform.Translate(Vector3.up * Time.deltaTime * velocitatCamara);
-        else if ((Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.UpArrow)) && transform.position.y > limitBaix) transform.Translate(Vector3.down * Time.deltaTime * velocitatCamara);
+        else if ((Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) && transform.position.y > limitBaix) transform.Translate(Vector3.down * Time.deltaTime * velocitatCamara);
         if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) && transform.position.x > limitEsquerra) transform.Translate(Vector3.left * Time.deltaTime * velocitatCamara);
         else if ((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) && transform.position.x < limitDreta) transform.Translate(Vector3.right * Time.deltaTime * velocitatCamara);
 
@@ -24,7 +24,11 @@ public class MovCamara : MonoBehaviour {
         else if (Input.GetAxis("Mouse ScrollWheel") < 0f && transform.position.z > limitEndarrera) // backward
             transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - velocitatCamara * .08f);
 
-        if (Input.GetKey(KeyCode.Q) && transform.position.z > limitEndarrera) transform.Translate(Vector3.back * Time.deltaTime * velocitatCamara);
-        else if (Input.GetKey(KeyCode.E) && transform.position.z < limitEndavant) transform.Translate(Vector3.forward * Time.deltaTime * velocitatCamara);
+        //    if (Input.GetKey(KeyCode.Q) && transform.position.z > limitEndarrera) //transform.Translate(Vector3.back * Time.deltaTime * velocitatCamara);
+        //else if (Input.GetKey(KeyCode.E) && transform.position.z < limitEndavant) transform.Translate(Vector3.forward * Time.deltaTime * velocitatCamara);
+        if (Input.GetKey(KeyCode.Q) && gameObject.GetComponent<Camera>().orthographicSize < 14)
+            gameObject.GetComponent<Camera>().orthographicSize += 0.1f;
+        else if (Input.GetKey(KeyCode.E) && gameObject.GetComponent<Camera>().orthographicSize > 4)
+            gameObject.GetComponent<Camera>().orthographicSize -= 0.1f;
     }
 }
