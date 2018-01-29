1cf711f098d9b50e96e5c4fa86ce3aca6c810865
Sergi Cugat <sergi.cugat@estudiants.urv.cat>
2018-01-29 22:38:07 +0100

    Nou mapa i menu principal afegits2

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour {
+    private static int mapa;
+
+    public int getMapa()
+    {
+        return mapa;
+    }
+
+    public void SetMapa1()
+    {
+        mapa = 0;
+    }
+
+    public void SetMapa2()
+    {
+        mapa = 1;
+    }
+}
---
+++ Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+
+    public GameObject mapa1;
+    public GameObject mapa2;
+    public Color colorMapaEscollit;
+
+    private void Start()
+    {
+        SetMapa1Button();
+    }
+
+    public void OnePlayerMode()
+    {
+        GameObject.Find("NetworkManager").GetComponent<NetworkManagerHUD>().enabled = false;
+        SceneManager.LoadScene("JocVsIa");
+    }
+
+    public void SetMapa1Button()
+    {
+        mapa1.GetComponent<Image>().color = colorMapaEscollit;
+        mapa2.GetComponent<Image>().color = Color.white;
+    }
+
+    public void SetMapa2Button()
+    {
+        mapa1.GetComponent<Image>().color = Color.white;
+        mapa2.GetComponent<Image>().color = colorMapaEscollit;
+    }
+}
